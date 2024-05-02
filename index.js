6fec4c95e24c27c9b182d3f0a2d5a72b38b966aa
Kurniawan Satria <satganzdevs@gmail.com>
2024-05-02 16:33:36 +0000

    aaa

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, './index.html'));
+res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.get('/messages', (req, res) => {
-const messages = JSON.parse(fs.readFileSync('./messages.json'));
+const messages = JSON.parse(fs.readFileSync(path.join(__dirname, '/messages.json')));
 res.json({status:200,messages:messages});
 });
 
@@ -22,14 +22,14 @@ const { message } = req.body;
 if (!message) {
 return res.status(400).send('Message is required');
 }
-const messages = JSON.parse(fs.readFileSync('./messages.json'));
+const messages = JSON.parse(fs.readFileSync(path.join(__dirname, './messages.json')));
 messages.push(message);
-fs.writeFileSync('./messages.json', JSON.stringify(messages, null, 2));
+fs.writeFileSync(path.join(__dirname, '/messages.json'), JSON.stringify(messages, null, 2));
 res.redirect('/');
 });
 
 app.get('/delete/messages', (req, res) => {
-fs.writeFileSync('./messages.json', '[]');
+fs.writeFileSync(path.join(__dirname, '/messages.json'), '[]');
 res.json({ status: 200, message: 'All messages deleted successfully' });
 });
 
